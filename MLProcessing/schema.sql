ef18e1a0f56f6bdcd38689d2a75700423d5db8ff
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 15:04:53 +0200

    test of SQL export

--- MLProcessing/schema.sql
+++ MLProcessing/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model MLProcessing of the subject dataModel.MachineLearning for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE MLProcessing_type AS ENUM ('MLProcessing');
-CREATE TABLE MLProcessing (alternateName TEXT, connectionParameters JSON, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, dueDate TIMESTAMP, name TEXT, objective TEXT, owner JSON, source TEXT, type MLProcessing_type);
+CREATE TABLE MLProcessing (alternateName TEXT, connectionParameters JSON, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, dueDate TIMESTAMP, id TEXT PRIMARY KEY, name TEXT, objective TEXT, owner JSON, seeAlso JSON, source TEXT, type MLProcessing_type);
