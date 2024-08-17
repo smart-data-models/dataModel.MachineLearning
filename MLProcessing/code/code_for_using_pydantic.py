231cb7b7035b3258120eb437a99b7ad78786aa5f
Alberto Abella <alberto.abella@fiware.org>
2024-08-18 00:47:01 +0200

    Draft version of pydantic file v2

---
+++ MLProcessing/code/code_for_using_pydantic.py
@@ -0,0 +1,105 @@
+from __future__ import annotations
+
+from enum import Enum
+from typing import List, Optional, Union
+
+from pydantic import AnyUrl, AwareDatetime, BaseModel, Field, constr
+
+
+class ConnectionParameters(BaseModel):
+    port: Optional[float] = None
+    server: Optional[str] = None
+    user: Optional[str] = None
+
+
+class Type(Enum):
+    MLProcessing = 'MLProcessing'
+
+
+class MLProcessing(BaseModel):
+    alternateName: Optional[str] = Field(
+        None, description='An alternative name for this item'
+    )
+    connectionParameters: Optional[ConnectionParameters] = Field(
+        None, description='Parameters for running the connection with the system'
+    )
+    dataProvider: Optional[str] = Field(
+        None,
+        description='A sequence of characters identifying the provider of the harmonised data entity',
+    )
+    dateCreated: Optional[AwareDatetime] = Field(
+        None,
+        description='Entity creation timestamp. This will usually be allocated by the storage platform',
+    )
+    dateModified: Optional[AwareDatetime] = Field(
+        None,
+        description='Timestamp of the last modification of the entity. This will usually be allocated by the storage platform',
+    )
+    description: Optional[str] = Field(None, description='A description of this item')
+    dueDate: Optional[AwareDatetime] = Field(
+        None,
+        description='Expected data / time for running the processing (for later versions, to allow planned processings)',
+    )
+    id: Optional[
+        Union[
+            constr(
+                pattern=r'^[\\w\\-\\.\\{\\}\\$\\+\\*\\[\\]`|~^@!, :\\\\]+$',
+                min_length=1,
+                max_length=256,
+            ),
+            AnyUrl,
+        ]
+    ] = Field(None, description='Unique identifier of the entity')
+    name: Optional[str] = Field(None, description='The name of this item')
+    objective: Optional[str] = Field(None, description='Objective of the processing')
+    owner: Optional[
+        List[
+            Union[
+                constr(
+                    pattern=r'^[\\w\\-\\.\\{\\}\\$\\+\\*\\[\\]`|~^@!,:\\\\]+$',
+                    min_length=1,
+                    max_length=256,
+                ),
+                AnyUrl,
+            ]
+        ]
+    ] = Field(
+        None,
+        description='A List containing a JSON encoded sequence of characters referencing the unique Ids of the owner(s)',
+    )
+    refMLModel: Optional[
+        Union[
+            constr(
+                pattern=r'^[\\w\\-\\.\\{\\}\\$\\+\\*\\[\\]`|~^@!, :\\\\]+$',
+                min_length=1,
+                max_length=256,
+            ),
+            AnyUrl,
+        ]
+    ] = Field(
+        None,
+        description='Relationship targeting the Machine Learning model to use for the processing',
+    )
+    refSubscriptionQuery: Optional[
+        Union[
+            constr(
+                pattern=r'^[\\w\\-\\.\\{\\}\\$\\+\\*\\[\\]`|~^@!, :\\\\]+$',
+                min_length=1,
+                max_length=256,
+            ),
+            AnyUrl,
+        ]
+    ] = Field(
+        None,
+        description='Relationship targeting the subscription query to use by the Machine Learning model',
+    )
+    seeAlso: Optional[Union[List[AnyUrl], AnyUrl]] = Field(
+        None, description='list of uri pointing to additional resources about the item'
+    )
+    source: Optional[str] = Field(
+        None,
+        description='A sequence of characters giving the original source of the entity data as a URL. Recommended to be the fully qualified domain name of the source provider, or the URL to the source object',
+    )
+    type: Optional[Type] = Field(
+        None, description='NGSI entity type. It has to be MLProcessing'
+    )
